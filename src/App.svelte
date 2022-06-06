975533f13c1f47e24014ff0b5ddd3149a03a887d
m.moghadam <mahyar.moghadam@outlook.com>
2022-06-06 15:56:57 +0430

    init

---
+++ src/App.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+	import './global.scss';
+	import { Router, Link, Route } from 'svelte-routing';
+	import Home from './routes/home.svelte';
+	import About from './routes/about.svelte';
+
+	export let url = '';
+</script>
+
+<Router {url}>
+	<nav>
+		<Link to="/">Home</Link>
+		<Link to="about">About</Link>
+		<Link to="blog">Blog</Link>
+	</nav>
+	<div>
+		<Route path="about" component={About} />
+		<Route path="/"><Home /></Route>
+	</div>
+</Router>
---
+++ src/routes/home.svelte
@@ -0,0 +1,3 @@
+<section>
+	<p>hello world !</p>
+</section>
