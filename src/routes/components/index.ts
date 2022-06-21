dca71ee69853559bbb69b4754010f8776c76b77a
m.moghadam <mahyar.moghadam@outlook.com>
2022-06-21 18:11:47 +0430

    change index

--- src/routes/components/index.ts
+++ src/routes/components/index.ts
@@ -1,2 +1,3 @@
 export { default as Footer } from './footer/footer.svelte'
 export { default as Header } from './header/header.svelte'
+export { default as Navbar } from './navbar/navbar.svelte'
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,9 +1,9 @@
 <script context="module" lang="ts">
-	export const prerender = true
 </script>
 
 <script lang="ts">
 	import { Button } from '$lib/components'
+	import Card from '$lib/components/card/Card.svelte'
 </script>
 
 <svelte:head>
@@ -16,12 +16,39 @@
 		<span class="welcome" />
 	</h1>
 
-	<Button color="primary">Primary</Button>
-	<Button color="secondary">Secondary</Button>
-	<Button color="success">Success</Button>
-	<Button color="warning">Warning</Button>
-	<Button color="danger">Danger</Button>
-	<Button color="info">Info</Button>
-	<Button color="light">Light</Button>
-	<Button color="dark">Dark</Button>
+	<Card title="Standard Buttons">
+		<div slot="body">
+			<div class="row g-2 align-items-center">
+				<div class="col-12 col-xl-2 font-weight-semibold">Normal</div>
+				<div class="col-6 col-sm-4 col-md-2 col-xl py-3">
+					<Button color="primary" block className="test">Primary</Button>
+				</div>
+				<div class="col-6 col-sm-4 col-md-2 col-xl py-3">
+					<Button color="secondary" block>Secondary</Button>
+				</div>
+				<div class="col-6 col-sm-4 col-md-2 col-xl py-3">
+					<Button color="success" block>Success</Button>
+				</div>
+
+				<div class="col-6 col-sm-4 col-md-2 col-xl py-3">
+					<Button color="warning" block>Warning</Button>
+				</div>
+
+				<div class="col-6 col-sm-4 col-md-2 col-xl py-3">
+					<Button color="danger" block>Danger</Button>
+				</div>
+				<div class="col-6 col-sm-4 col-md-2 col-xl py-3">
+					<Button color="info" block>Info</Button>
+				</div>
+
+				<div class="col-6 col-sm-4 col-md-2 col-xl py-3">
+					<Button color="dark" block>Dark</Button>
+				</div>
+
+				<div class="col-6 col-sm-4 col-md-2 col-xl bg-dark py-3">
+					<Button color="light" block>Light</Button>
+				</div>
+			</div>
+		</div>
+	</Card>
 </section>
