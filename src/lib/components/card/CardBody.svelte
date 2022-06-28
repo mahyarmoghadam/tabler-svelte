941d337361214f3061a6f4867010b43e0f65ef80
farshadGholamali <f.gholamali@dadekavanco.onmicrosoft.com>
2022-06-28 16:31:34 +0430

    #1 feat(card) :  add card title option to card body

--- src/lib/components/card/CardBody.svelte
+++ src/lib/components/card/CardBody.svelte
@@ -1,3 +1,15 @@
+<script lang="ts">
+	import CardTitle from './CardTitle.svelte'
+	export let title: string = ''
+</script>
+
 <div class="card-body">
+	{#if title}
+		<CardTitle>
+			<slot name="title">
+				{title}
+			</slot>
+		</CardTitle>
+	{/if}
 	<slot />
 </div>
