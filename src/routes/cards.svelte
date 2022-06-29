37890804447b8e8e9b38f2cd062c35e5550230a6
farshadGholamali <farshadgholamali@gmail.com>
2022-06-29 11:36:33 +0430

    feat(card) : update card page with new card

--- src/routes/cards.svelte
+++ src/routes/cards.svelte
@@ -1,11 +1,13 @@
 <script lang="ts">
 	import Card from '$lib/components/card/Card.svelte'
 	import CardBody from '$lib/components/card/CardBody.svelte'
+	import CardHeader from '$lib/components/card/CardHeader.svelte'
 	import Col from '$lib/components/grid/Col.svelte'
 	import Row from '$lib/components/grid/Row.svelte'
 	let title = 'Card Title'
 	let headerLight = true
-	let borderless=true
+	let borderless = true
+	let subtitle = 'Subtitle'
 </script>
 
 <Row>
@@ -21,9 +23,14 @@
 	</Col>
 	<Col col="3">
 		<Card {borderless}>
-			<CardBody {title} >
-			<div>Card without border</div>
+			<CardBody {title}>
+				<div>Card without border</div>
 			</CardBody>
+		</Card>
+	</Col>
+	<Col col="3">
+		<Card {title} {subtitle}>
+			<CardBody>Card with title and subtitle</CardBody>
 		</Card>
 	</Col>
 </Row>
