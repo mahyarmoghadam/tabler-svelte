d49382f7bf581ce2d055bf5959cc0fac411f958d
farshadGholamali <farshadgholamali@gmail.com>
2022-06-29 15:59:38 +0430

    feat(card) : update card page

--- src/routes/cards.svelte
+++ src/routes/cards.svelte
@@ -1,14 +1,15 @@
 <script lang="ts">
 	import Card from '$lib/components/card/Card.svelte'
 	import CardBody from '$lib/components/card/CardBody.svelte'
-	import CardHeader from '$lib/components/card/CardHeader.svelte'
 	import Col from '$lib/components/grid/Col.svelte'
 	import Row from '$lib/components/grid/Row.svelte'
+	
 	let title = 'Card Title'
 	let headerLight = true
 	let borderless = true
 	let subtitle = 'Subtitle'
 	let toHref = '#'
+	let linkRotate = true
 </script>
 
 <Row>
@@ -39,6 +40,11 @@
 	<Col col="3">
 		<Card {toHref}>
 			<CardBody>Default hover effect</CardBody>
+		</Card>
+	</Col>
+	<Col col="3">
+		<Card {toHref} {linkRotate}>
+			<CardBody>Rotate hover effect</CardBody>
 		</Card>
 	</Col>
 </Row>
