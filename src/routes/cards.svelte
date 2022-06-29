cb90a31b458882efa19f32e4947f35ad4123300f
farshadGholamali <farshadgholamali@gmail.com>
2022-06-29 16:04:22 +0430

    feat(card) : update card page

--- src/routes/cards.svelte
+++ src/routes/cards.svelte
@@ -3,13 +3,14 @@
 	import CardBody from '$lib/components/card/CardBody.svelte'
 	import Col from '$lib/components/grid/Col.svelte'
 	import Row from '$lib/components/grid/Row.svelte'
-	
+
 	let title = 'Card Title'
 	let headerLight = true
 	let borderless = true
 	let subtitle = 'Subtitle'
 	let toHref = '#'
 	let linkRotate = true
+	let linkPop = true
 </script>
 
 <Row>
@@ -45,6 +46,11 @@
 	<Col col="3">
 		<Card {toHref} {linkRotate}>
 			<CardBody>Rotate hover effect</CardBody>
+		</Card>
+	</Col>
+	<Col col="3">
+		<Card {toHref} {linkPop}>
+			<CardBody>Pop hover effect</CardBody>
 		</Card>
 	</Col>
 </Row>
