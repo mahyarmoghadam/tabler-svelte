29db14b31b3ab059c5e2a4e13913b7ae253607e9
Farshad gholamali <98264671+farshadGholamali@users.noreply.github.com>
2022-06-30 15:29:25 +0430

    feat(card) : update card page

--- src/routes/cards.svelte
+++ src/routes/cards.svelte
@@ -15,6 +15,8 @@
 	let rotateLeft = true
 	let cardActive = true
 	let cardInactive = true
+	let bgStamp = 'yellow'
+	let textMuted = true
 </script>
 
 <Row>
@@ -80,6 +82,31 @@
 		<Card {cardInactive}>
 			<CardBody>
 				<p>This is some text inactive state.</p>
+			</CardBody>
+		</Card>
+	</Col>
+	<Col col="3">
+		<Card {bgStamp}>
+			<svg
+				slot="stamp"
+				xmlns="http://www.w3.org/2000/svg"
+				class="icon"
+				width="24"
+				height="24"
+				viewBox="0 0 24 24"
+				stroke-width="2"
+				stroke="currentColor"
+				fill="none"
+				stroke-linecap="round"
+				stroke-linejoin="round">
+				<path stroke="none" d="M0 0h24v24H0z" fill="none" />
+				<path
+					d="M10 5a2 2 0 0 1 4 0a7 7 0 0 1 4 6v3a4 4 0 0 0 2 3h-16a4 4 0 0 0 2 -3v-3a7 7 0 0 1 4 -6" />
+				<path d="M9 17v1a3 3 0 0 0 6 0v-1" />
+			</svg>
+			<CardBody {title} {textMuted}>
+				Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto at consectetur culpa
+				ducimus eum fuga fugiat, ipsa iusto, modi nostrum recusandae reiciendis saepe.
 			</CardBody>
 		</Card>
 	</Col>
