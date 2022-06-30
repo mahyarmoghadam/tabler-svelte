bba38ac468a1908c015e04b9b5709bda1a15c5ef
Farshad gholamali <98264671+farshadGholamali@users.noreply.github.com>
2022-06-30 17:12:42 +0430

    feat(card) : update card page

--- src/routes/cards.svelte
+++ src/routes/cards.svelte
@@ -18,6 +18,7 @@
 	let bgStamp = 'yellow'
 	let textMuted = true
 	let bgCard = 'primary-lt'
+	let txtStamp = 'primary'
 </script>
 
 <Row>
@@ -116,6 +117,32 @@
 			<CardBody title={'Card with primary light background'} {textMuted}>
 				Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto at consectetur culpa
 				ducimus eum fuga fugiat, ipsa iusto, modi nostrum recusandae reiciendis saepe.
+			</CardBody>
+		</Card>
+	</Col>
+	<Col col="3">
+		<Card bgStamp={'white'} {txtStamp} bgCard={'primary'}>
+			<svg
+				slot="stamp"
+				xmlns="http://www.w3.org/2000/svg"
+				class="icon"
+				width="24"
+				height="24"
+				viewBox="0 0 24 24"
+				stroke-width="2"
+				stroke="currentColor"
+				fill="none"
+				stroke-linecap="round"
+				stroke-linejoin="round">
+				<path stroke="none" d="M0 0h24v24H0z" fill="none" />
+				<path
+					d="M12 17.75l-6.172 3.245l1.179 -6.873l-5 -4.867l6.9 -1l3.086 -6.253l3.086 6.253l6.9 1l-5 4.867l1.179 6.873z" />
+			</svg>
+			<CardBody title={'Card with background and icon'}>
+				<p>
+					Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto at consectetur culpa
+					ducimus eum fuga fugiat, ipsa iusto, modi nostrum recusandae reiciendis saepe.
+				</p>
 			</CardBody>
 		</Card>
 	</Col>
