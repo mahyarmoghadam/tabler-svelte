ae03e273e67ef7b915a4e91317c55d6b3c06e456
Farshad gholamali <98264671+farshadGholamali@users.noreply.github.com>
2022-07-01 12:24:20 +0430

    #1 refactor(card): refactor card footer component

--- src/lib/components/card/CardFooter.svelte
+++ src/lib/components/card/CardFooter.svelte
@@ -3,15 +3,9 @@
 </script>
 
 <div class="card-footer">
-	<!-- TODO: not standard -->
-	<div class="row d-flex align-items-center">
-		{#if footer}
-			<div class="col-auto">
-				{footer}
-			</div>
-		{/if}
-		<div class="col-auto ms-auto">
-			<slot />
-		</div>
-	</div>
+	{#if footer}
+		{footer}
+	{/if}
+
+	<slot />
 </div>
