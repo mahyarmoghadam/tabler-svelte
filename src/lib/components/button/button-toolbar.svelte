4d6cd7180666be6166ab200470403ce737d71263
Mahyar Moghadam <mahyar.moghadam@outlook.com>
2022-07-07 14:36:01 +0530

    add button toolbar

---
+++ src/lib/components/button/button-toolbar.svelte
@@ -0,0 +1,11 @@
+<script>
+	import clsx from 'clsx'
+
+	let className = ''
+	export { className as class }
+	$: classes = clsx(className, 'btn-toolbar')
+</script>
+
+<div {...$$restProps} role="toolbar" class={classes}>
+	<slot />
+</div>
--- src/lib/components/button/index.ts
+++ src/lib/components/button/index.ts
@@ -1,2 +1,3 @@
 export { default as Button } from './button.svelte'
 export { default as ButtonGroup } from './button-group.svelte'
+export { default as ButtonToolbar } from './button-toolbar.svelte'
