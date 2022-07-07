3ce5454aaa100121454d6fbb7f9ec1a634a55b2e
Farshad gholamali <98264671+farshadGholamali@users.noreply.github.com>
2022-07-07 19:12:51 +0430

    feat(card): add more class to CardBody component

--- src/lib/components/card/CardBody.svelte
+++ src/lib/components/card/CardBody.svelte
@@ -1,10 +1,16 @@
 <script lang="ts">
+	import clsx from 'clsx'
+
 	import CardTitle from './CardTitle.svelte'
+
 	export let title: string = ''
 	export let textMuted: boolean = false
+	export let className: string = ''
+
+	$: classes = clsx('card-body', { [className]: className })
 </script>
 
-<div class="card-body">
+<div class={classes}>
 	{#if title}
 		<CardTitle>
 			<slot name="title">
